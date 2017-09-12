ff7dd4f1107141d95fb5e49c8b2b03613bdba837
fernandoTavella <ferd.tavella@gmail.com>
2017-09-12 19:34:00 -0300

    Presentacion TP
    modificada por el profe en clase

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,9 +1,10 @@
 
+import java.awt.GridLayout;
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 import anotaciones.TTextArea;
@@ -24,23 +25,30 @@ public class Prueba {
 		
 		//Muestra en base a los atributos de la clase
 		Field[] fields = ShowAnotacion.class.getDeclaredFields();
+		
+		JFrame frame = new JFrame("Test");
+		frame.setLayout(new GridLayout(5,1));
+		
+		
 		for(Field f : fields){
 		      if (f.isAnnotationPresent(TTextArea.class)){
 		    	  TTextArea ta = f.getAnnotation(TTextArea.class);
-		    	  JFrame frame = new JFrame("Test");
 				  frame.setVisible(true);
 				  frame.setSize(500,200);
 				  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				  
-				  JPanel panel = new JPanel();
+				  
+				  JLabel lbl = new JLabel(ta.label());
+				  frame.getContentPane().add(lbl);
+				  
+				  
 				  JTextArea text = new JTextArea();
 				  text.setVisible(true);
-				  text.setEditable(false);
+//				  text.setEditable(false);
 				  text.setLocation(0,20);
-				  text.setText(ta.label());
-				  panel.add(text);
+//				  text.setText(ta.label());
 				  
-				  frame.add(panel);
+				  frame.getContentPane().add(new JScrollPane(text));
 				  
 				  System.out.println(ta.label());
 		      }
