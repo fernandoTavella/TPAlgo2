8927cccdcdcaf75fbb694b0f10b7d007efb0ec9a
fernandoTavella <ferd.tavella@gmail.com>
2017-09-20 09:24:06 -0300

    refactor del codigo

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,7 +1,10 @@
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -17,110 +20,82 @@ public class Prueba {
 	
 	public static void main (String[] arr){
 
-		//Muestra en base a los metodos de la clase
-//	  Method[] methods = ShowAnotacion.class.getMethods();
-//	  for (Method m : methods){
-//	          if (m.isAnnotationPresent(TTextArea.class))
-//	      {
-//	        	  TTextArea ta = m.getAnnotation(TTextArea.class);
-//	          System.out.println(ta.label());
-//	      }
-//	  }
-		
+		JFrame frame = new JFrame("Main");
+		frame.setLayout(new GridLayout(5,1));
+		frame.setVisible(true);
+		frame.setSize(2200,200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//Muestra en base a los atributos de la clase
 		Field[] fields = ShowAnotacion.class.getDeclaredFields();
-		
-		JFrame frame = new JFrame("Test");
-		frame.setLayout(new GridLayout(5,1));
-		
-		JButton boton = new JButton("boton");
-		Field[] b1 = MostrarBoton.class.getDeclaredFields();
-			
-		for(Field b : b1){
-			if (b.isAnnotationPresent(BotonAnnotation.class)){
-				BotonAnnotation botonazo = b.getAnnotation(BotonAnnotation.class);
-				boton.setVisible(true);
-				float posicionX = botonazo.posHorizontal();
-				float posicionY= botonazo.posVertical();
-				int ancho = botonazo.ancho();
-				int largo = botonazo.largo();
-				String nombre = botonazo.nombre();
-				Dimension tamaño= new Dimension(ancho,largo);
-				boton.setAlignmentX(posicionX);
-				boton.setAlignmentY(posicionY);
-				boton.setSize(tamaño);
-				boton.setLabel(nombre);
-				frame.getContentPane().add(boton);
-				
-				
-				
-			}
-		}
 				
 		for(Field f : fields){
 		      if (f.isAnnotationPresent(TTextArea.class)){
 		    	  TTextArea ta = f.getAnnotation(TTextArea.class);
-				  frame.setVisible(true);
-				  frame.setSize(2200,200);
-				  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				  
-				  
-				  JLabel lbl = new JLabel(ta.label());
+		    	  JLabel lbl = new JLabel(ta.label());				  
 				  frame.getContentPane().add(lbl);
-				  
-				  
 				  JTextArea text = new JTextArea();
 				  text.setVisible(true);
-//				  text.setEditable(false);
 				  text.setLocation(0,20);
-//				  text.setText(ta.label());
-				  
+				  text.setSize(500,60);
 				  frame.getContentPane().add(new JScrollPane(text));
-				  
-				  System.out.println(ta.label());
 		      }
-		}
-		
-		
-		JRadioButton radioButton = new JRadioButton("radio");
-		Field[] bandera = MostrarRadio.class.getDeclaredFields();
-		
-		for(Field ban : bandera){
-		      if (ban.isAnnotationPresent(RadioButtonAnnotation.class)){
-		    	  RadioButtonAnnotation radio = ban.getAnnotation(RadioButtonAnnotation.class);
-				  ban.setAccessible(true);
-				  String nombre = radio.nombre();
-				  radioButton.setLabel(nombre);
-				  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		  	if (f.isAnnotationPresent(RadioButtonAnnotation.class)){
+		    	  RadioButtonAnnotation radio = f.getAnnotation(RadioButtonAnnotation.class);
+		  		  JRadioButton radioButton = new JRadioButton(radio.nombre());
+				  f.setAccessible(true);
 				  frame.getContentPane().add(radioButton);
 		      }
-		}  
+		}
+		//Muestra en base a los metodos de la clase
+		  Method[] methods = ShowAnotacion.class.getMethods();
+		  for (Method m : methods){
+		          if (m.isAnnotationPresent(TTextArea.class)){}
+				  	if (m.isAnnotationPresent(BotonAnnotation.class)){
+						BotonAnnotation botonazo = m.getAnnotation(BotonAnnotation.class);
+						JButton boton = new JButton(botonazo.nombre());
+						boton.setVisible(true);
+						float posicionX = botonazo.posHorizontal();
+						float posicionY= botonazo.posVertical();
+						int ancho = botonazo.ancho();
+						int largo = botonazo.largo();
+						Dimension tamaño= new Dimension(ancho,largo);
+						boton.setAlignmentX(posicionX);
+						boton.setAlignmentY(posicionY);
+						boton.setSize(tamaño);
+						boton.addActionListener(new ActionListener()
+						{
+							@Override
+							public void actionPerformed(ActionEvent e)
+							{
+								System.out.println("show");
+								
+							}
+						});
+						frame.getContentPane().add(boton);
+					}
+		  }
 	}
 }
 
 class ShowAnotacion{
 	
-	@TTextArea(label="ahora se ve en base al field")
+	@TTextArea(label="Text Area")
 	private String probando;
 	
 	//@TTextArea(label="input aca")
 	public void showAnottation(){}
 	
-
-}
-
-class MostrarBoton {
 	@BotonAnnotation(nombre = "botonazo",posHorizontal=1, posVertical=2, ancho=2, largo=1)
-	private int a;
+	public void mostrarBoton(){
+		System.out.println("boton click");
+	}
 	
-	public void mostrarBoton(){}
-	
-}
-
-class MostrarRadio {
-	@RadioButtonAnnotation(nombre = "radio button")
+	@RadioButtonAnnotation(nombre = "es alumno")
 	private boolean ban;
 	
-	public void mostrarRadio(){}
+	public void mostrarRadio(){
+		
+	}
 	
 }
--- src/anotaciones/BotonAnnotation.java
+++ src/anotaciones/BotonAnnotation.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
+@Target(ElementType.METHOD)
 
 
 public @interface BotonAnnotation
